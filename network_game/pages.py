6726273de51604f1d1a5b7f3b14472563635f4b5
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2020-02-19 13:27:30 +0100

    layout more or less done

--- network_game/pages.py
+++ network_game/pages.py
@@ -78,8 +78,7 @@ class Game(Page):
             # else:
             #     ties_rem_options[player.id_in_group] = p.ties_rem_options.split()
             if player.id_in_group == 1:
-                ties_rem_options[player.id_in_group] = "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 " \
-                                                       "16 17 18 19 20 21 22 23".split()
+                ties_rem_options[player.id_in_group] = "1 2 3 4 5 6 7 8 9".split()
             else:
                 ties_rem_options[player.id_in_group] = "3 5".split()
 
