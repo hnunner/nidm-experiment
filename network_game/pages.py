0c96aaecf4c307282b38d1715fb51b577ecaaa94
Nunner,H. (Hendrik) <h.nunner@students.uu.nl>
2020-02-26 10:07:10 +0100

    added mock data for non-clustered networks

--- network_game/pages.py
+++ network_game/pages.py
@@ -57,74 +57,147 @@ class Game(Page):
 
             # disease_states[player.id_in_group] = p.disease_state
 
+            # if player.id_in_group == 1:
+            #     ties[player.id_in_group] = "2 3 8 12 15 20 23 24".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 2:
+            #     ties[player.id_in_group] = "5 7 11 14 16 18 21".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 3:
+            #     ties[player.id_in_group] = "4 8 12 15 20 23 24".split()
+            #     disease_states[player.id_in_group] = 2
+            # elif player.id_in_group == 4:
+            #     ties[player.id_in_group] = "6 9 10 13 17 19".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 5:
+            #     ties[player.id_in_group] = "7 11 14 16 18 20 21".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 6:
+            #     ties[player.id_in_group] = "9 10 13 17 19 22".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 7:
+            #     ties[player.id_in_group] = "11 14 16 18 21 22".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 8:
+            #     ties[player.id_in_group] = "12 15 20 22 23 24".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 9:
+            #     ties[player.id_in_group] = "10 13 17 19 22".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 10:
+            #     ties[player.id_in_group] = "13 17 19".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 11:
+            #     ties[player.id_in_group] = "13 14 16 18 21".split()
+            #     disease_states[player.id_in_group] = 2
+            # elif player.id_in_group == 12:
+            #     ties[player.id_in_group] = "15 20 22 23 24".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 13:
+            #     ties[player.id_in_group] = "17 19".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 14:
+            #     ties[player.id_in_group] = "16 17 18 21".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 15:
+            #     ties[player.id_in_group] = "19 20 23 24".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 16:
+            #     ties[player.id_in_group] = "18 21 22".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 17:
+            #     ties[player.id_in_group] = "19".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 18:
+            #     ties[player.id_in_group] = "21".split()
+            #     disease_states[player.id_in_group] = 2
+            # elif player.id_in_group == 19:
+            #     ties[player.id_in_group] = "".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 20:
+            #     ties[player.id_in_group] = "23 24".split()
+            #     disease_states[player.id_in_group] = 3
+            # elif player.id_in_group == 21:
+            #     ties[player.id_in_group] = "22".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 22:
+            #     ties[player.id_in_group] = "24".split()
+            #     disease_states[player.id_in_group] = 3
+            # elif player.id_in_group == 23:
+            #     ties[player.id_in_group] = "24".split()
+            #     disease_states[player.id_in_group] = 1
+            # elif player.id_in_group == 24:
+            #     ties[player.id_in_group] = "".split()
+            #     disease_states[player.id_in_group] = 3
+
             if player.id_in_group == 1:
-                ties[player.id_in_group] = "2 3 8 12 15 20 23 24".split()
+                ties[player.id_in_group] = "4 5 8 17 18 19 20 24".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 2:
-                ties[player.id_in_group] = "5 7 11 14 16 18 21".split()
+                ties[player.id_in_group] = "4 7 9 10 13 15 23".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 3:
-                ties[player.id_in_group] = "4 8 12 15 20 23 24".split()
+                ties[player.id_in_group] = "4 5 7 9 12 13 15".split()
                 disease_states[player.id_in_group] = 2
             elif player.id_in_group == 4:
-                ties[player.id_in_group] = "6 9 10 13 17 19".split()
+                ties[player.id_in_group] = "7 8 12 21 22".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 5:
-                ties[player.id_in_group] = "7 11 14 16 18 20 21".split()
+                ties[player.id_in_group] = "7 10 11 21 22 24".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 6:
-                ties[player.id_in_group] = "9 10 13 17 19 22".split()
+                ties[player.id_in_group] = "9 13 14 15 17 23 24".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 7:
-                ties[player.id_in_group] = "11 14 16 18 21 22".split()
+                ties[player.id_in_group] = "18 19 23 24".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 8:
-                ties[player.id_in_group] = "12 15 20 22 23 24".split()
+                ties[player.id_in_group] = "9 13 17 19 23 24".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 9:
-                ties[player.id_in_group] = "10 13 17 19 22".split()
+                ties[player.id_in_group] = "11 14 16 22".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 10:
-                ties[player.id_in_group] = "13 17 19".split()
+                ties[player.id_in_group] = "12 17 18 19 20 23".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 11:
-                ties[player.id_in_group] = "13 14 16 18 21".split()
+                ties[player.id_in_group] = "14 15 17 20 23".split()
                 disease_states[player.id_in_group] = 2
             elif player.id_in_group == 12:
-                ties[player.id_in_group] = "15 20 22 23 24".split()
+                ties[player.id_in_group] = "14 16 19 22 24".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 13:
-                ties[player.id_in_group] = "17 19".split()
+                ties[player.id_in_group] = "16 17 18 20".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 14:
-                ties[player.id_in_group] = "16 17 18 21".split()
+                ties[player.id_in_group] = "15 18 23".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 15:
-                ties[player.id_in_group] = "19 20 23 24".split()
+                ties[player.id_in_group] = "16 19 21".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 16:
-                ties[player.id_in_group] = "18 21 22".split()
+                ties[player.id_in_group] = "17 20 21 23".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 17:
-                ties[player.id_in_group] = "19".split()
+                ties[player.id_in_group] = "22".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 18:
-                ties[player.id_in_group] = "21".split()
+                ties[player.id_in_group] = "20 21 22".split()
                 disease_states[player.id_in_group] = 2
             elif player.id_in_group == 19:
-                ties[player.id_in_group] = "".split()
+                ties[player.id_in_group] = "21 22".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 20:
-                ties[player.id_in_group] = "23 24".split()
+                ties[player.id_in_group] = "21".split()
                 disease_states[player.id_in_group] = 3
             elif player.id_in_group == 21:
-                ties[player.id_in_group] = "22".split()
+                ties[player.id_in_group] = "24".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 22:
                 ties[player.id_in_group] = "24".split()
                 disease_states[player.id_in_group] = 3
             elif player.id_in_group == 23:
-                ties[player.id_in_group] = "24".split()
+                ties[player.id_in_group] = "".split()
                 disease_states[player.id_in_group] = 1
             elif player.id_in_group == 24:
                 ties[player.id_in_group] = "".split()
